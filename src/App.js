e15425317be03f42a6b7c9061d29ffbcb5306933
sudhareddypam1919 <114248870+sudhareddypam1919@users.noreply.github.com>
2022-09-23 21:54:46 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello World</h1>
       <p>This is my first application in react js</p>
     </div>
   );
