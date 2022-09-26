9c2427de509334ea5da20a6056c4668d3a824df7
sudhareddypam1919 <114248870+sudhareddypam1919@users.noreply.github.com>
2022-09-26 11:07:05 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
 
-export function App() {
+export const App = () => {
   return (
     <div>
       <h1>Hello Sudha</h1>
     </div>
   );
-}
+};
